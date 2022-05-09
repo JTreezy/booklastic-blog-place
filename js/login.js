f1b110d97d4401e0c264652004ea75b8335c387a
Andrew Ryu <andrewryu@outlook.com>
2022-05-09 11:34:37 -0700

    pulling Haley's update

--- js/login.js
+++ js/login.js
@@ -1,19 +1,20 @@
 console.log("login linked1")
-document.querySelector("#login").addEventListener("submit",e=>{
+document.querySelector("#loginButton").addEventListener("submit",e=>{
     e.preventDefault();
     const userObj = {
-        username:document.querySelector("#loginUsername").value,
-        password:document.querySelector("#loginPassword").value,
+        username:document.querySelector("#loginuser").value,
+        password:document.querySelector("#loginpass").value,
     }
     console.log(userObj)
-    fetch("/api/users/login",{
+    fetch("users/login",{
         method:"POST",
         body:JSON.stringify(userObj),
         headers:{
             "Content-Type":"application/json"
         }
     }).then(res=>{
-        if(res.ok){
+        if(res.ok){  //200 status code
+            console.log("logged in")
             location.href="/profile"
         } else {
             alert("trumpet sound")
@@ -21,24 +22,24 @@ document.querySelector("#login").addEventListener("submit",e=>{
     })
 })
 
-document.querySelector("#signup").addEventListener("submit",e=>{
-    e.preventDefault();
-    const userObj = {
-        username:document.querySelector("#signupUsername").value,
-        password:document.querySelector("#signupPassword").value,
-    }
-    console.log(userObj)
-    fetch("/api/users/",{
-        method:"POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>{
-        if(res.ok){
-            location.href="/profile"
-        } else {
-            alert("trumpet sound")
-        }
-    })
-})
+// document.querySelector("#signup").addEventListener("submit",e=>{
+//     e.preventDefault();
+//     const userObj = {
+//         username:document.querySelector("#signupUsername").value,
+//         password:document.querySelector("#signupPassword").value,
+//     }
+//     console.log(userObj)
+//     fetch("/api/users/",{
+//         method:"POST",
+//         body:JSON.stringify(userObj),
+//         headers:{
+//             "Content-Type":"application/json"
+//         }
+//     }).then(res=>{
+//         if(res.ok){
+//             location.href="/profile"
+//         } else {
+//             alert("trumpet sound")
+//         }
+//     })
+// })
